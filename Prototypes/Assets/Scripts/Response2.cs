925b9c6b5b8ae0045c54ae28c16513042ad35f58
Scott316 <57153117+Scott316@users.noreply.github.com>
2019-10-29 19:41:08 +0000

    Add files via upload

---
+++ Prototypes/Assets/Scripts/Response2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Response1 : MonoBehaviour
+{
+    public Slider slider;
+    public float trust = 50;
+    public Button mybutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button response = mybutton.GetComponent<Button>();
+        response.onClick.AddListener(TaskButtonClick);
+
+    }
+
+    void TaskButtonClick()
+    {
+        trust -= 50f;
+        slider.value = trust;
+
+    }
+}
