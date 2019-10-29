276bb768f356b67fa3e58701f3ea41ea2b6f3660
StevenMewhirter <45992104+StevenMewhirter@users.noreply.github.com>
2019-10-29 23:36:25 +0000

    social media prototype

---
+++ Prototypes/Assets/Scripts/MediaPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MediaPlayer : MonoBehaviour
+{
+
+    public GameObject ImageOnPanel;  ///set this in the inspector
+    public Texture NewTexture;
+    public Texture NewTexture1;
+    public Texture NewTexture2;
+    public Texture NewTexture3;
+    private RawImage img;
+    public GameObject Response1;
+    public GameObject Response2;
+
+    void Start()
+    {
+        Response1.gameObject.SetActive(false);// hides the panel on canvas
+        Response2.gameObject.SetActive(false);//hides the panel on canvas
+        img = (RawImage)ImageOnPanel.GetComponent<RawImage>();
+            StartCoroutine(Time());
+
+       // img.texture = (Texture)NewTexture;
+    }
+
+  
+    IEnumerator Time()
+    {
+        yield return new WaitForSeconds(3); // waits 3 seconds
+        img.texture = (Texture)NewTexture;
+        yield return new WaitForSeconds(3); // waits 3 seconds
+        img.texture = (Texture)NewTexture1;
+        yield return new WaitForSeconds(3); // waits 3 seconds
+        Response1.gameObject.SetActive(true);// Shows the panel on canvas
+        Response2.gameObject.SetActive(true);// Shows the panel on canvas
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+             Response1.gameObject.SetActive(false);// hides the panel on canvas
+             Response2.gameObject.SetActive(false);//hides the panel on canvas
+            img.texture = (Texture)NewTexture2;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Response1.gameObject.SetActive(false);// hides the panel on canvas
+            Response2.gameObject.SetActive(false);//hides the panel on canvas
+            img.texture = (Texture)NewTexture3;
+        }
+    }
+}
+
