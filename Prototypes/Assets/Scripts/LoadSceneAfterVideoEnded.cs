79cf01bb2226a2583721fe0fac25d4b5091d65a5
Scott316 <57153117+Scott316@users.noreply.github.com>
2019-10-31 12:37:06 +0000

    Add files via upload

---
+++ Prototypes/Assets/Scripts/LoadSceneAfterVideoEnded.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAfterVideoEnded : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer; 
+    public string SceneName;
+    void Start()
+    {
+        VideoPlayer.loopPointReached += EndReached;
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene (SceneName);
+    }
+}
