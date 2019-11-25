e8075c52c8ce3041b110003c7e64295f33dda27d
StevenMewhirter <45992104+StevenMewhirter@users.noreply.github.com>
2019-11-25 23:58:50 +0000

    Reltaionship meter across Scenes

---
+++ Prototypes/Assets/Scripts/RelationshipSlider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelationshipSlider : MonoBehaviour
+{
+    public Slider RobinSlider;
+    public Button RobinB;
+    public static float RobinMeter;
+    void Start()
+    {
+        RobinMeter = 50f;
+        RobinSlider.value = RobinMeter;
+        
+        Button response = RobinB.GetComponent<Button>();
+        response.onClick.AddListener(RelationshipR);
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        RobinSlider.value = RobinMeter;
+
+    }
+    void RelationshipR()
+    {
+        RobinMeter += 10f;
+
+    }
+}
---
+++ Prototypes/Assets/Scripts/Scene7Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene7Script : MonoBehaviour
+{
+    public Slider RobinSlide;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // RelationshipSlider.RobinMeter = RobinSlide.value; // saving data
+        RobinSlide.value = RelationshipSlider.RobinMeter; // load stuff
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
