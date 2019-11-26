4f29890517cb6cf008f12d57b6b2c37d938b8f19
StevenMewhirter <45992104+StevenMewhirter@users.noreply.github.com>
2019-11-26 10:40:01 +0000

    Put scripts into folders

---
+++ Prototypes/Assets/Scripts/TestScripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{//sets the timer to 20 seconds
+    public float timer = 20f;
+    //declares a Game Object called TimerUI
+    public GameObject TimerUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //starts the countdown of the timer
+        timer -= Time.deltaTime;
+   
+
+        //if the timer reaches 0'
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
