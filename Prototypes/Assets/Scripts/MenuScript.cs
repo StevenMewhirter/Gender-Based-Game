886ca1e9c9f5c2c17be738f3eca964d9983425d0
ruaridh.stevenson <44837932+rsteve208@users.noreply.github.com>
2020-03-25 18:21:01 +0000

    Menus added

---
+++ Prototypes/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject mainMenuScreen;
+    public GameObject newGameScreen;
+    public GameObject chapterSelectScreen;
+    public GameObject optionsScreen;
+
+    public RawImage logo;
+    public Text header;
+
+    public Button back;
+    public Button options;
+
+    public Button newGameButton;
+    public Button LoadGameButton;
+
+    public Button newStoryButton;
+    public Button chapterSelectButton;
+
+    public Button katieButton;
+    public Button robinButton;
+    public Button aminaButton;
+
+    public Canvas warningOverlay;
+    public Button yesButton;
+    public Button noButton;
+
+
+    void Awake()
+    {
+        // Resets menu to main screen
+        mainMenuScreen.SetActive(true);
+        newGameScreen.SetActive(false);
+        chapterSelectScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+
+        logo.gameObject.SetActive(true);
+        header.gameObject.SetActive(true);
+
+        back.gameObject.SetActive(false);
+        options.gameObject.SetActive(true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Enables relevant buttons for each screen
+   public void MainMenuScreen()
+    {
+        mainMenuScreen.SetActive(true);
+        newGameScreen.SetActive(false);
+        chapterSelectScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+
+        logo.gameObject.SetActive(true);
+        header.gameObject.SetActive(true);
+
+        back.gameObject.SetActive(false);
+        options.gameObject.SetActive(true);
+    }
+
+   public void NewGameScreen()
+    { 
+        // If there is a save game, the WarningOverlay canvas will display on top of the screen 
+        
+        mainMenuScreen.SetActive(false);
+        newGameScreen.SetActive(true);
+        chapterSelectScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+
+        logo.gameObject.SetActive(true);
+        header.gameObject.SetActive(false);
+
+        back.gameObject.SetActive(true);
+        options.gameObject.SetActive(true);
+    }
+
+   public void ChapterSelectScreen()
+    {
+        mainMenuScreen.SetActive(false);
+        newGameScreen.SetActive(false);
+        chapterSelectScreen.SetActive(true);
+        optionsScreen.SetActive(false);
+
+        logo.gameObject.SetActive(false);
+        header.gameObject.SetActive(false);
+
+        back.gameObject.SetActive(true);
+        options.gameObject.SetActive(false);
+    }
+
+   public void OptionsScreen()
+    {
+        mainMenuScreen.SetActive(false);
+        newGameScreen.SetActive(false);
+        chapterSelectScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+
+        logo.gameObject.SetActive(false);
+        header.gameObject.SetActive(true);
+
+        back.gameObject.SetActive(true);
+        options.gameObject.SetActive(false);
+    }
+}
