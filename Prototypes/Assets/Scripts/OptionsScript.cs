60d5a2449d52f2c9a2f1c1950af2a9cc87f8ce72
ruaridh.stevenson <44837932+rsteve208@users.noreply.github.com>
2020-04-08 20:34:21 +0100

    Basic Options Menu stuff

--- Prototypes/Assets/Scripts/OptionsScript.cs
+++ Prototypes/Assets/Scripts/OptionsScript.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class OptionsScript : MonoBehaviour
 {
+    public bool audioSetting;
+    public bool vibrateSetting;
+    public bool colourBlindSetting;
+
+    // 1 = slow, 2 = medium, 3 = fast
+    public int textSpeedSetting;
+
+    public static GameObject audioManager;
+
     // Start is called before the first frame update
-    void Start()
+    void awake()
     {
-        
+        audioSetting = true;
+        vibrateSetting = true;
+        colourBlindSetting = false;
+        textSpeedSetting = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ChangeAudioSettings()
+    {
+        if (audioSetting == false)
+        {
+            audioSetting = true;
+        }
+        else
+        {
+            audioSetting = false;
+        }
+    }
+
+    void ChangeVibrateSettings()
+    {
+        if (vibrateSetting == false)
+        {
+            vibrateSetting = true;
+        }
+        else
+        {
+            vibrateSetting = false;
+        }
     }
 }
