a18d53ee1345162228b1c4aff77d115a8cc46e30
ElpAlex <52038964+ElpAlex@users.noreply.github.com>
2020-04-15 20:09:58 +0100

    Scene 2 text aligned
    Created a new script (TextAligned) that changes the font size and aligns the text to the center of the text box.
    Size of the font changed to 25 from 30.
    "Excuse me" option changed in the test.txt file to fit the text box better

---
+++ Prototypes/Assets/Scripts/TextAligned.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAligned : MonoBehaviour
+{
+    //this an empty gameobject (child of the text box gameobject) where the text is going to be moved to, so it always appears in the correct position
+    public GameObject textBoxCenter;
+    public Text thisText;
+    int thisFontSize = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisText.fontSize = thisFontSize;
+        thisText.alignment = TextAnchor.UpperLeft;
+        gameObject.transform.position = textBoxCenter.transform.position;
+    }
+}
