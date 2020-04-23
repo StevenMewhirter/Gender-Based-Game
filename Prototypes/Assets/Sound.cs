6a5cfd245c430ede76292cf1a4f9838dd958d9df
Scott316 <57153117+Scott316@users.noreply.github.com>
2020-04-23 13:09:35 +0100

    Added Audio Source to chapter 1 scene 1

---
+++ Prototypes/Assets/Sound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+
+    private static Sound instance = null;
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+         
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}    
